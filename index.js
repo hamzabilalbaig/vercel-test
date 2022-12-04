2f50eb50d75eb2914615059ccb7b0bafb7dbca53
Hamza <hamzabilalbaig@gmail.com>
2022-12-04 14:17:32 +0500

    m

--- index.js
+++ index.js
@@ -19,50 +19,50 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config({ path: "./backend/config/config.env" });
 }
 
-const connectDatabase = () => {
-  console.log(process.env.MONGO_URI);
-  mongoose
-    .connect(process.env.MONGO_URI)
-    .then((con) => console.log(`Database Connected: ${con.connection.host}`))
-    .catch((err) => console.log(err));
-};
+// const connectDatabase = () => {
+//   console.log(process.env.MONGO_URI);
+//   mongoose
+//     .connect(process.env.MONGO_URI)
+//     .then((con) => console.log(`Database Connected: ${con.connection.host}`))
+//     .catch((err) => console.log(err));
+// };
 
-connectDatabase();
+// connectDatabase();
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
 
-// Using Middlewares
+// // Using Middlewares
+// // app.use(express.json({ limit: "50mb" }));
+// // app.use(express.urlencoded({ limit: "50mb", extended: true }));
+// // app.use(cookieParser());
+// // app.use(fileUpload);
 // app.use(express.json({ limit: "50mb" }));
 // app.use(express.urlencoded({ limit: "50mb", extended: true }));
 // app.use(cookieParser());
-// app.use(fileUpload);
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use(cookieParser());
-app.use(
-  fileUpload({
-    limits: { fileSize: 50 * 1024 * 1024 },
-    useTempFiles: true,
-  })
-);
+// app.use(
+//   fileUpload({
+//     limits: { fileSize: 50 * 1024 * 1024 },
+//     useTempFiles: true,
+//   })
+// );
 
-app.use(cors());
+// app.use(cors());
 
-// Importing Routes
-const post = require("./backend/routes/post");
-const user = require("./backend/routes/user");
-const conversations = require("./backend/routes/conversations");
-const messages = require("./backend/routes/messages");
+// // Importing Routes
+// const post = require("./backend/routes/post");
+// const user = require("./backend/routes/user");
+// const conversations = require("./backend/routes/conversations");
+// const messages = require("./backend/routes/messages");
 
-// Using Routes
-app.use("/api/v1", post);
-app.use("/api/v1", user);
-app.use("/api/conversations", conversations);
-app.use("/api/messages", messages);
+// // Using Routes
+// app.use("/api/v1", post);
+// app.use("/api/v1", user);
+// app.use("/api/conversations", conversations);
+// app.use("/api/messages", messages);
 
 app.get("/", (req, res) => res.send("Hello World! whith backend"));
 app.listen(process.env.PORT, () =>
