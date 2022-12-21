62d5af0f9b334858224464d53630be9c8b6245ab
Hamza <hamzabilalbaig@gmail.com>
2022-12-21 18:28:48 +0500

    asd

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-app.use(express.json({ limit: "150mb" }));
+app.use(express.json({ limit: "111150mb" }));
 app.use(express.urlencoded({ limit: "150mb", extended: true }));
 app.use(
   express.raw({
