ca0e9bce41cb39c1bfbc1831427beef4adea317b
Hamza <hamzabilalbaig@gmail.com>
2022-12-21 19:48:25 +0500

    asd

--- backend/controllers/reel.js
+++ backend/controllers/reel.js
@@ -4,7 +4,7 @@ const cloudinary = require("cloudinary");
 
 exports.createReel = async (req, res) => {
   try {
-    const myCloud = await cloudinary.v2.uploader.upload_large(req.body.reel, {
+    const myCloud = await cloudinary.v2.uploader.upload(req.body.reel, {
       resource_type: "video",
       folder: "reels",
     });
